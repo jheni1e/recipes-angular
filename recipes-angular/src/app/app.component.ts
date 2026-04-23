4b615386a4671e7df0184b7ad5d3acb1cecd1085
Jhenie <highhalma>
2026-04-23 08:14:59 -0300

    clean imports in app component

--- recipes-angular/src/app/app.component.ts
+++ recipes-angular/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from './shared/button/button.component';
-import { HeaderComponent } from "./shared/header/header.component";
-import { SearchBarComponent } from "./shared/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
